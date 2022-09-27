ae71b450fa8c9966ceece0bd2a7d054e6dafb12f
kauezero <kauezero1@gmail.com>
2022-09-27 01:56:25 -0300

    Changed REST reques method

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'a0e9f7227ac4c7533db1');
+<?php return array('dependencies' => array('jquery'), 'version' => '89101b40417e89a1ce3f');
